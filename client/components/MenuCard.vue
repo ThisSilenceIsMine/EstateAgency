94c044c3b09de1797c34bb01e22c96261ffbb0b4
ThisSilenceIsMine <49694311+ThisSilenceIsMine@users.noreply.github.com>
2021-01-19 17:58:21 +0200

    Design is not my strongest side. Started working at home page

---
+++ client/components/MenuCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <sui-card>
+      <sui-dimmer-dimmable
+        @mouseenter.native="cardActive = true"
+        @mouseleave.native="cardActive = false"
+      >
+        <sui-image :src="src" />
+        <sui-dimmer blurring :active="cardActive">
+          <slot></slot>
+        </sui-dimmer>
+      </sui-dimmer-dimmable>
+    </sui-card>
+  </div>
+</template>
+<script>
+export default {
+  name: "menu-card",
+  props: {
+    src: String,
+  },
+  data() {
+    return {
+      cardActive: false,
+    };
+  },
+};
+</script>
+<style scoped>
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,73 +1,130 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        EstateAgency
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div id="nav">
+    <sui-container>
+      <sui-card-group :items-per-row="3" stackable>
+        <sui-card>
+          <sui-dimmer-dimmable
+            @mouseenter.native="cardOneActive = true"
+            @mouseleave.native="cardOneActive = false"
+          >
+            <div class="cardText" v-if="!cardOneActive">Для покупців</div>
+            <sui-image src="img/interior-1.jpg" />
+            <sui-dimmer blurring :active="cardOneActive">
+              <sui-button inverted>Go</sui-button>
+            </sui-dimmer>
+          </sui-dimmer-dimmable>
+        </sui-card>
+
+        <sui-card>
+          <sui-dimmer-dimmable
+            @mouseenter.native="cardTwoActive = true"
+            @mouseleave.native="cardTwoActive = false"
+          >
+            <div class="cardText" v-if="!cardTwoActive">Для власників</div>
+            <sui-image src="img/interior-2.jpg" />
+            <sui-dimmer blurring :active="cardTwoActive">
+              <sui-button inverted>Go</sui-button>
+            </sui-dimmer>
+          </sui-dimmer-dimmable>
+        </sui-card>
+
+        <sui-card>
+          <sui-dimmer-dimmable
+            @mouseenter.native="cardThreeActive = true"
+            @mouseleave.native="cardThreeActive = false"
+          >
+            <div class="cardText" v-if="!cardThreeActive">
+              Для орендаторів
+            </div>
+            <sui-image src="img/interior-3.jpg" />
+            <sui-dimmer blurring :active="cardThreeActive">
+              <sui-button inverted>Go</sui-button>
+            </sui-dimmer>
+          </sui-dimmer-dimmable>
+        </sui-card>
+      </sui-card-group>
+    </sui-container>
+    <sui-divider horizontal> Нові пропозиції </sui-divider>
+    <sui-container>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus maiores
+      dignissimos, voluptatem tempore obcaecati alias officia quia distinctio
+      voluptas fugiat corporis libero ducimus ex porro itaque eligendi magni
+      velit corrupti.
+    </sui-container>
+    <sui-divider horizontal> Про нас </sui-divider>
+    <sui-container>
+      <!-- about us -->
+      <sui-grid :columns="2" divided>
+        <sui-grid-row stretched>
+          <sui-grid-column>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+              aliquam reprehenderit asperiores id rem nobis explicabo blanditiis
+              delectus laudantium, natus odio libero nam tenetur ut maxime dolor
+              maiores itaque animi. Veniam similique impedit ea tempore laborum,
+              nihil adipisci facere autem obcaecati, odit natus iste vero
+              possimus. Provident at explicabo tenetur, quidem mollitia ullam
+              perferendis maiores excepturi, nesciunt culpa ducimus voluptatem.
+              Corrupti nisi, sapiente ut laudantium dolorum officiis illum dolor
+              beatae dignissimos accusamus, harum velit facere quidem distinctio
+              nemo ipsum aperiam corporis vitae tempore perspiciatis quas veniam
+              soluta tenetur. Inventore, eligendi? Neque fugit corporis
+              consequatur, iure similique dolores incidunt molestias illo quam
+              reiciendis provident. Mollitia fugiat quas incidunt. Omnis, ullam
+              nobis! Ducimus reprehenderit blanditiis sapiente dolorem nisi
+              repudiandae tempore aliquid pariatur!
+            </p>
+          </sui-grid-column>
+          <sui-grid-column>
+            <h1 is="sui-header">
+              Зворотній зв'язок
+            </h1>
+            <p>Відгуки та пропозиції: feedback@blagovist.ua</p>
+            <p>Комерційні пропозиції: reklama@comfort.ua</p>
+            <p>Прес-служба: press@comfort.ua</p>
+            <p>Редактор сайту: webeditor@comfort.ua</p>
+            <sui-button-group>
+              <sui-button circular social="facebook" icon="facebook" />
+              <sui-button circular social="twitter" icon="twitter" />
+              <sui-button circular social="linkedin" icon="linkedin" />
+              <sui-button circular social="google plus" icon="google plus" />
+            </sui-button-group>
+          </sui-grid-column>
+        </sui-grid-row>
+      </sui-grid>
+    </sui-container>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      cardOneActive: false,
+      cardTwoActive: false,
+      cardThreeActive: false
+    };
+  }
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+#nav {
+  margin-top: 1%;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+.cardText {
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -30%);
+  z-index: 5;
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  font-size: 35px;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  color: #4a707a;
 }
-
-.links {
-  padding-top: 15px;
+#about {
+  position: relative;
 }
 </style>
